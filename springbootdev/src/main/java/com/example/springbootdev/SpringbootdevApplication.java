55e827dd6dadc647dee6ae6d75e8d8fd05c28132
Ubuntu <Prajwal@springboot-vm.pr415r1obzeezcxcngjjat324g.hx.internal.cloudapp.net>
2025-01-30 06:02:17 +0000

    Initial commit

--- springbootdev/src/main/java/com/example/springbootdev/SpringbootdevApplication.java
+++ springbootdev/src/main/java/com/example/springbootdev/SpringbootdevApplication.java
@@ -2,12 +2,23 @@ package com.example.springbootdev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringbootdevApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootdevApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdevApplication.class, args);
+    }
 
+    @RestController
+    class HelloController {
+
+        @GetMapping("/hello")
+        public String sayHello() {
+            return "Hello, World!";
+        }
+    }
 }
+
