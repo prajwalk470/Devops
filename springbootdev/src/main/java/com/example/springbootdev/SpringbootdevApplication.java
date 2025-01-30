2c9945d5edbbf61184a087bfaa362a92de88b1c3
prajwal <prajwalkumatagi07@gmail.com>
2025-01-30 10:49:36 +0530

    Initial Spring devops project

---
+++ springbootdev/src/main/java/com/example/springbootdev/SpringbootdevApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdevApplication.class, args);
+	}
+
+}
